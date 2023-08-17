e3edff94dda64f3da105cd7dfde8e5df281b5930
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2023-08-18 00:23:46 +0700

    use firebase instead 😉

--- app.vue
+++ app.vue
@@ -1,5 +1,30 @@
+<script setup>
+import { signInAnonymously } from "firebase/auth"
+
+const appReady = computed(() => {
+  const user = useCurrentUser()
+  return !!user.value
+})
+
+onMounted(async () => {
+  try {
+    const currentUser = await getCurrentUser()
+
+    if (!currentUser) {
+      const auth = useFirebaseAuth()
+      await signInAnonymously(auth)
+    }
+  } catch (error) {
+    console.log(error)
+  }
+})
+</script>
+
 <template>
-  <NuxtPage />
+  <NuxtPage v-if="appReady" />
+  <div class="h-full flex justify-center items-center" v-else>
+    <div class="i-eos-icons-bubble-loading text-5xl" />
+  </div>
 </template>
 
 <style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,22 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@unocss/nuxt", "@vueuse/nuxt"],
+  modules: ["@unocss/nuxt", "@vueuse/nuxt", "nuxt-vuefire"],
   css: ["@unocss/reset/tailwind.css"],
   runtimeConfig: {
     public: {},
+  },
+  ssr: false,
+  vuefire: {
+    auth: true,
+    config: {
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      databaseURL: process.env.FIREBASE_DATABASE_URL,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
+      measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+    },
   },
 });
