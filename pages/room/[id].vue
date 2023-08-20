e00c1c63d3c06c1bff0d22e60130ed384f40ba61
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2023-08-21 00:53:48 +0700

    waiting room / lobby

--- pages/room/[id].vue
+++ pages/room/[id].vue
@@ -1,78 +1,49 @@
 <script setup lang="ts">
-import { Suit } from "@/utils/card"
-
 const router = useRouter()
 const route = useRoute()
 const { player } = usePlayer()
 
 const id = route.params.id
-const count = ref(0)
 
 if (!player.value) {
   router.push("/")
 }
 
-const ranks = [
-  "A",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "J",
-  "Q",
-  "K",
-]
-
-const suits = [
-  Suit.Diamonds,
-  Suit.Clubs,
-  Suit.Hearts,
-  Suit.Spades,
-]
-
-const deck: { rank: string; suit: Suit }[] = []
-ranks.forEach((rank) => {
-  suits.forEach((suit) => {
-    deck.push({ rank, suit })
-  })
-})
-
-const shuffledDeck = deck
-  .map((card) => ({ ...card, order: Math.random() }))
-  .sort((a, b) => a.order - b.order)
-  .map((card) => ({ rank: card.rank, suit: card.suit }))
-
 import { ref as dbRef, set } from "firebase/database";
 
 const db = useDatabase();
-const roomRef = dbRef(db, `rooms/${id}`);
+const waitingRoomRef = dbRef(db, `rooms/${id}/waitingRoom`);
+const playerStatusRef = dbRef(db, `rooms/${id}/waitingRoom/${player.value!.id}`);
 
-const room = useDatabaseObject<any>(roomRef);
+const waitingRoom = useDatabaseObject<any>(waitingRoomRef);
+const playerStatus = useDatabaseObject<any>(playerStatusRef);
 
-watch(room, (value) => {
-  if (value) {
-    count.value = Object.keys(value.waitingRoom).length;
+const isReady = computed<boolean>(() => {
+  return !!playerStatus.value?.ready;
+})
+
+const players = computed<any[]>(() => {
+  if (waitingRoom.value) {
+    return Object.values(waitingRoom.value);
   }
-}, { immediate: true })
+  return [];
+})
 
-watch(player, value => {
-  if (value) {
-    const waitingRoomRef = dbRef(db, `rooms/${id}/waitingRoom/${value.id}`);
-    set(waitingRoomRef, true);
-  }
-}, { immediate: true })
+async function updateWaitingRoom(data: any) {
+  await set(playerStatusRef, data);
+}
+
+async function updateWaitingRoomStatus(ready = false) {
+  await updateWaitingRoom({
+    name: player.value!.name,
+    ready,
+  });
+}
+
+updateWaitingRoomStatus()
 
 async function exit() {
-  if (player.value) {
-    const waitingRoomRef = dbRef(db, `rooms/${id}/waitingRoom/${player.value.id}`);
-    await set(waitingRoomRef, null);
-  }
-
+  await updateWaitingRoom(null);
   router.push("/");
 }
 </script>
@@ -84,12 +55,31 @@ async function exit() {
     </div>
     <div class="mb-4">
       <span>Room #{{ id }}</span>
-      <span v-if="count >= 4" class="font-bold text-green-500 ml-1">(Ready)</span>
-      <span v-else-if="count" class="font-bold text-red-500 ml-1">({{ count - 4 }})</span>
     </div>
-    <div>Hello, {{ player?.name }}</div>
-    <div class="grid grid-cols-13 gap-2 max-w-[1130px]">
-      <CardPokerCard v-for="card in shuffledDeck" :rank="card.rank" :suit="card.suit" />
+    <div>
+      <span class="mr-2">Ready?</span>
+      <button class="py-0.5 px-2 text-sm" :class="isReady ? 'bg-green' : 'bg-neutral-300'"
+        @click="updateWaitingRoomStatus(true)">Yes</button>
+      /
+      <button class="py-0.5 px-2 text-sm" :class="isReady ? 'bg-neutral-300' : 'bg-red'"
+        @click="updateWaitingRoomStatus(false)">No</button>
+    </div>
+    <div>
+      <div class="flex mt-10 items-center">
+        <div class="text-8xl pr-6 text-red-600">
+          {{ players.length - 4 }}
+        </div>
+        <ol class="list-decimal pl-10">
+          <li v-for="i in 4">
+            <div v-if="!players[i - 1]" class="text-black/40">waiting...</div>
+            <div v-else class="flex items-center gap-1">
+              {{ players[i - 1].name }}
+              <div v-if="players[i - 1].ready" class="i-ph-check-circle-bold text-green-700 inline-block" />
+              <div v-else class="i-eos-icons-bubble-loading text-neutral-500 inline-block" />
+            </div>
+          </li>
+        </ol>
+      </div>
     </div>
   </ClientOnly>
 </template>
